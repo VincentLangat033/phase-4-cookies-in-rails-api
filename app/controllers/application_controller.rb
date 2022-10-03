e70b49b335670a1e265f155b4497a2f871eb46f1
Vincent Langat <kimutailangat033@gmail.com>
2022-10-03 05:51:44 +0300

    cookies in rails api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  # include the action controller :: cookies so that cookies can be used anywhere in your app
 end
